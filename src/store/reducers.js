6351d90683d8d86d7b092f4933e529da393f000d
Jonathan Cole <jonathancole@Jonathans-MacBook-Pro.local>
2021-01-25 11:24:16 -0500

    added store/reducers.js

---
+++ src/store/reducers.js
@@ -0,0 +1,12 @@
+function web3(state = {}, action) {
+    switch (action.type) {
+      default:
+        return state
+    }
+  }
+
+  const rootReducer = combineReducers({
+    web3
+  })
+
+  export default rootReducer
