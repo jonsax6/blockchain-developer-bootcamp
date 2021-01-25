c8b4243609d40802e6576b1f210bdb0391dc2d12
Jonathan Cole <jonathancole@Jonathans-MacBook-Pro.local>
2021-01-25 14:31:12 -0500

    redux devtools

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import Web3 from 'web3';
 import Token from '../abis/Token.json';
+import { loadWeb3 } from '../store/interactions'
 
 class App extends Component {
   componentWillMount() {
-    this.loadBlockchainData()
+    this.loadBlockchainData(this.props.dispatch)
   }
 
-  async loadBlockchainData() {
-    const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545')
+  async loadBlockchainData(dispatch) {
+    const web3 = loadWeb3(dispatch)
     const network = await web3.eth.net.getNetworkType()
     const networkId = await web3.eth.net.getId()
     const accounts = await web3.eth.getAccounts();
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,3 +1,5 @@
+import { combineReducers } from 'redux';
+
 function web3(state = {}, action) {
     switch (action.type) {
       default:
